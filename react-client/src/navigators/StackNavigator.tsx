bb82229c0c47afd88b57933d7c61b9585e0ca524
jwilla <toe.johan@gmail.com>
2022-12-12 19:51:00 -0500

    styles

--- react-client/src/navigators/StackNavigator.tsx
+++ react-client/src/navigators/StackNavigator.tsx
@@ -4,10 +4,22 @@ const Stack = createNativeStackNavigator()
 
 import React from 'react'
 import BookingList from '../features/bookingList/page/BookingList'
+import { colors, styles } from '../theme/theme'
 
 const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle:{
+          backgroundColor: colors.primary
+        },
+        headerShadowVisible: false,
+        headerTitleStyle:{
+          ...styles.title,
+          color: colors.secondary
+        },
+      }}
+    >
       <Stack.Screen name='bookings' component={BookingList}/>
     </Stack.Navigator>
   )
