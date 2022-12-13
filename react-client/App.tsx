b5e450096b4a207b4ad8f10f1feab4c824dd8a2e
jwilla <toe.johan@gmail.com>
2022-12-12 22:53:54 -0500

    statusbar

--- react-client/App.tsx
+++ react-client/App.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'react-native';
 import StackNavigator from './src/navigators/StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
       <StackNavigator />
+      <StatusBar/>
     </NavigationContainer>
   );
 }
