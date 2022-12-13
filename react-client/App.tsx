5480b6b7dbaaa83077aa091e926458ce603e8eb7
jwilla <toe.johan@gmail.com>
2022-12-12 21:24:10 -0500

    update

--- react-client/App.tsx
+++ react-client/App.tsx
@@ -1,22 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import StackNavigator from './src/navigators/StackNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={customStyles.container}>
-        <StackNavigator />
-        <StatusBar style="auto" />
-      </View>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
-
-const customStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
