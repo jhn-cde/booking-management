6864efce73e8cab2c5f4eb698e531aad463befe4
jwilla <toe.johan@gmail.com>
2022-12-16 22:21:14 -0500

    status

--- react-client/src/ts/interfaces/booking.interface.tsx
+++ react-client/src/ts/interfaces/booking.interface.tsx
@@ -1,15 +1,9 @@
-enum EState{
-  Pending = "Pending",
-  InProgress = "In Progress",
-  Completed = "Completed",
-  Cancelled = "Cancelled",
-}
 export interface BookingInterface {
   startdate: Date,
   tours: {name: String, _id: String}[],
   contact: {name: String, _id: String},
   ntravelers: Number,
-  state: EState,
+  state: "Pending" | "In Progress" | "Completed" | "Cancelled",
   price: Number,
   foodType: String,
   travelers: {_id: String}[],
