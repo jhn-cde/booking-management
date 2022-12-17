839ea3357db33ca5a61ce96fb46fee9e38c20293
jwilla <toe.johan@gmail.com>
2022-12-16 22:23:30 -0500

    styles

--- react-client/src/features/booking/components/BookingForm.tsx
+++ react-client/src/features/booking/components/BookingForm.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { useAppSelector } from '../../../app/hooks'
+import data from '../../../assets/countries.json'
+import MyPicker from '../../../components/MyPicker'
+import useForm from '../../../hooks/useForm'
+import { selectColors } from '../../../theme/themeSlice'
+import { BookingInterface } from '../../../ts/interfaces/booking.interface'
 
-const BookingForm = () => {
-  
+const BookingForm = ({initialState}: {initialState: BookingInterface}) => {
+  const [booking, handleInputChange] = useForm(initialState)
+  const colors = useAppSelector(selectColors);
+  const countries = data.countries
+
   return (
-    <View></View>
+    <View 
+      style={{
+        ...customStyles.elementContainer, 
+        backgroundColor: colors.border,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+      <MyPicker
+        options={countries} 
+        initialValue={'Peru'}
+        changeValue={(newValue) => {}}
+      />
+    </View>
   )
 }
+
+const customStyles = StyleSheet.create({
+  elementContainer:{
+    padding: 10,
+    borderRadius: 10
+  }
+})
 
 export default BookingForm
