f9578ee83d2ce5a22656fa42b2be1617770f145b
jwilla <toe.johan@gmail.com>
2022-12-16 19:12:49 -0500

    refresh component

---
+++ react-client/src/components/Refresh.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import Icon from '@expo/vector-icons/Ionicons';
+import { selectColors } from '../theme/themeSlice';
+import { useAppSelector } from '../app/hooks';
+
+const Refresh = ({refreshFun}:{refreshFun: () => void}) => {
+  const colors = useAppSelector(selectColors);
+  return (
+    <View
+      style={{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        width:150, 
+        alignItems:'center',
+        marginVertical: 20
+      }}
+    >
+      <Text
+        style={{color:colors.text}}
+      >Cargando...</Text>
+      <TouchableOpacity
+        onPress={refreshFun}
+      >
+        <Icon
+          name="refresh" 
+          color={colors.card} 
+          size={26}
+          style={{textDecorationLine: 'underline'}}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Refresh
