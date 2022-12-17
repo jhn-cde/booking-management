d47d9f8454a60e7f38d22080e7172650e9162171
jwilla <toe.johan@gmail.com>
2022-12-16 19:34:44 -0500

    title

--- react-client/src/features/booking/page/Booking.tsx
+++ react-client/src/features/booking/page/Booking.tsx
@@ -1,28 +1,25 @@
 import React, { useEffect } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
-import axios from 'axios';
-import { BookingInterface } from '../../../ts/interfaces/booking.interface'
-import useForm from '../../../hooks/useForm';
 import { RootStackScreenProps } from '../../../navigators/types';
 import PageContainer from '../../../components/PageContainer';
 import AddBooking from '../components/AddBooking';
 import EditBooking from '../components/EditBooking';
 
 const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) => {
-  if(route.params){
-    return (
-      <PageContainer>
-        <EditBooking id={route.params.id}/>
-      </PageContainer>
-    )
-  }
-  else{
-    return (
-      <PageContainer>
-        <AddBooking />
-      </PageContainer>
-    )
-  }
+
+  useEffect(() => {
+    const title = route.params? 'Reserva' : 'Agregar reserva' 
+    navigation.setOptions({title: title})
+  }, [route.params])
+  
+
+  return (
+    <PageContainer>
+      { route.params
+        ?<EditBooking id={route.params.id}/>
+        :<AddBooking />
+      }
+    </PageContainer>
+  )
 }
 
 export default Booking
