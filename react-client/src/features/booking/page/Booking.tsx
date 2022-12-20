5e3d0befb57577bc7ead9658a2f0eff42d9e2597
jwilla <toe.johan@gmail.com>
2022-12-20 13:38:47 -0500

    edit state

--- react-client/src/features/booking/page/Booking.tsx
+++ react-client/src/features/booking/page/Booking.tsx
@@ -1,22 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { RootStackScreenProps } from '../../../navigators/types';
 import PageContainer from '../../../components/PageContainer';
 import AddBooking from '../components/AddBooking';
 import EditBooking from '../components/EditBooking';
 
 const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) => {
-
-  useEffect(() => {
-    const title = route.params? 'Reserva' : 'Agregar reserva' 
-    navigation.setOptions({title: title})
-  }, [route.params])
-  
-
+  const [edit, setEdit] = useState(false)
   return (
     <PageContainer>
-      { route.params
-        ?<EditBooking id={route.params.id}/>
-        :<AddBooking />
+      { edit&&
+        <EditBooking id={route.params.id}/>
       }
     </PageContainer>
   )
