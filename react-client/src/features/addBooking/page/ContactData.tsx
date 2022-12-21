3e9d3a0d298d252762573d31a231ef52ed2466f4
jwilla <toe.johan@gmail.com>
2022-12-21 11:11:58 -0500

    use customer form

--- react-client/src/features/addBooking/page/ContactData.tsx
+++ react-client/src/features/addBooking/page/ContactData.tsx
@@ -1,119 +1,17 @@
 import React from 'react'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
-import FormItemContainer from '../../../components/FormItemContainer'
-import useForm from '../../../hooks/useForm'
-import { styles } from '../../../theme/theme'
-import { selectColors } from '../../../theme/themeSlice';
-import { useSelector } from 'react-redux';
-import CustomInput from '../../../components/CustomInput'
 import PageContainer from '../../../components/PageContainer'
 import { AddBookingScreenProps } from '../navigator/types'
+import CustomerForm from '../components/CustomerForm'
+import { CustomerInterface } from '../../../ts/interfaces/customer.interface'
 
-const ContactData = ({navigation, route}: AddBookingScreenProps<'Contact'>) => {
-  const colors = useSelector(selectColors);
-  const [contact, handleChange] = useForm({
-    name:'',
-    ndoc:''
-  })
+const ContactData = ({navigation}: AddBookingScreenProps<'Contact'>) => {
+  const onSubmit = (customer: CustomerInterface) => {
+    navigation.navigate('Tours')
+  }
 
   return (
     <PageContainer>
-      <View style={{flex:1}}>
-        <Text
-          style={styles.subtitle}
-        >
-          Datos de contacto
-        </Text>
-
-        <View>
-          <FormItemContainer
-            name={'Nombres*'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'person'}
-              placeholder={'Nombres'}
-              handleInputChange={(val) => handleChange({k:'name', v:val})}
-            />
-          </FormItemContainer>
-
-          <FormItemContainer
-            name={'N. doc*'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'clipboard'}
-              placeholder={'N. doc'}
-              handleInputChange={(val) => handleChange({k:'ndoc', v:val})}
-            />
-          </FormItemContainer>
-
-          <FormItemContainer
-            name={'Email'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'mail'}
-              placeholder={'Email'}
-              handleInputChange={(val) => handleChange({k:'email', v:val})}
-            />
-          </FormItemContainer>
-
-          <FormItemContainer
-            name={'Phone'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'call'}
-              placeholder={'Phone'}
-              handleInputChange={(val) => handleChange({k:'phone', v:val})}
-            />
-          </FormItemContainer>
-
-          <FormItemContainer
-            name={'Pais'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'locate'}
-              placeholder={'Pais'}
-              handleInputChange={(val) => handleChange({k:'nationality', v:val})}
-            />
-          </FormItemContainer>
-
-          <FormItemContainer
-            name={'Fecha de nacimiento'}
-          >
-            <CustomInput
-              value={contact.name}
-              iconName={'calendar'}
-              placeholder={'Fecha de nacimiento'}
-              handleInputChange={(val) => handleChange({k:'birthdate', v:val})}
-            />
-          </FormItemContainer>
-        </View>
-
-        <View style={{flex:1, flexDirection:'row', justifyContent:'flex-end', alignItems: 'center'}}>
-          <TouchableOpacity
-            style={{
-              borderRadius: 6,
-              backgroundColor: colors.secondary,
-              paddingVertical: 10,
-              paddingHorizontal: 20,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            onPress={()=>
-              navigation.navigate('Tours')}
-          >
-            <Text
-              style={{fontSize: 20, color:'#fff'}}
-            >
-              Siguiente
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <CustomerForm onSubmit={onSubmit}/>
     </PageContainer>
   )
 }
