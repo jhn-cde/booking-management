2f0e1feb51ed43aa795463a280cd88a5a0615970
jwilla <toe.johan@gmail.com>
2023-01-03 15:46:54 -0500

    dekete AddBooking

--- react-client/src/features/booking/page/Booking.tsx
+++ react-client/src/features/booking/page/Booking.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { RootStackScreenProps } from '../../../navigators/types';
 import PageContainer from '../../../components/PageContainer';
-import AddBooking from '../components/AddBooking';
 import EditBooking from '../components/EditBooking';
 
 const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) => {
