dde86dfaee602146eef711db2c9b967b0f25cb73
jwilla <toe.johan@gmail.com>
2023-01-03 16:00:03 -0500

    toursI removed

--- react-client/src/features/addBooking/api/ToursApi.tsx
+++ react-client/src/features/addBooking/api/ToursApi.tsx
@@ -1,8 +1,7 @@
 import { api } from "../../../api/api";
-import { toursI } from "../components/ManageTours";
 
 export const fetchTours = (setTours: (value: React.SetStateAction<any[] | undefined>) => void) => {
-  api.get<toursI[]>('/tours')
+  api.get<any[]>('/tours')
   .then(response => {
     if(response.status === 200){
       const data = response.data.map(option => {return {label: option.name, value: option._id}})
