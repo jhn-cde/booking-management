a0da06d695237389953773d4bcbb6dd33fdb5e1c
jwilla <toe.johan@gmail.com>
2023-01-03 15:45:00 -0500

    user friendly

--- react-client/src/components/CustomDateTimePicker.tsx
+++ react-client/src/components/CustomDateTimePicker.tsx
@@ -1,6 +1,6 @@
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
 import React, { useState } from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/theme';
 
 interface Props{
@@ -10,34 +10,84 @@ interface Props{
 
 const CustomDateTimePicker = ({date, setDate}:Props) => {
   const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState('1')
 
   const onChange = (event: DateTimePickerEvent, date: Date | undefined) => {
     if(date){
       const currentDate = date;
       setShow(false);
       setDate(currentDate);
+      setSelected('3')
     }
   };
 
   return (
-    <TouchableOpacity
-      onPress={()=>setShow(!show)}
-      style={{...styles.input, width: '80%'}}
+    <View style={{
+      width:'auto',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'}}
     >
-      <Text style={{...styles.input, borderBottomWidth: 0, width: '100%'}}>
-        {date.toLocaleDateString('es-PE')}
-      </Text>
+      <TouchableOpacity
+        onPress={()=>{
+          setDate(new Date())
+          setSelected('1')
+        }}
+        style={selected === '1'?customStyles.btnSelected:customStyles.btn}
+      >
+        <Text style={selected === '1'?customStyles.selected:customStyles.text}> Hoy </Text>
+      </TouchableOpacity>
 
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          is24Hour={true}
-          onChange={onChange}
-        />
-      )}
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={()=>{
+          let tomorrow = new Date()
+          tomorrow.setDate(tomorrow.getDate()+1)
+          setDate(tomorrow)
+          setSelected('2')
+        }}
+        style={selected === '2'?customStyles.btnSelected:customStyles.btn}
+      >
+        <Text style={selected === '2'?customStyles.selected:customStyles.text}> Mañana </Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity
+        onPress={()=>setShow(!show)}
+        style={selected === '3'?customStyles.btnSelected:customStyles.btn}
+      >
+        <Text style={selected === '3'?customStyles.selected:customStyles.text}> Otro día... </Text>
+
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            is24Hour={true}
+            onChange={onChange}
+          />
+        )}
+      </TouchableOpacity>
+    </View>
   )
 }
+
+const customStyles = StyleSheet.create({
+  btn: {
+    padding: 5,
+    borderRadius: 5,
+    borderBottomWidth: 1,
+    marginEnd: 5
+  },
+  btnSelected:{
+    padding: 5,
+    borderRadius: 5,
+    marginEnd: 5
+  },
+  text: {
+    fontSize: 14
+  },
+  selected: {
+    fontSize: 18,
+    color: 'green'
+  }
+})
 
 export default CustomDateTimePicker
