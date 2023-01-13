7acbd6800f8ad51a1df1cb613fd0bff364066d19
jwilla <140156@unsaac.edu.pe>
2023-01-13 09:02:21 -0500

    material menu

--- react-client/src/components/KebabBtn.tsx
+++ react-client/src/components/KebabBtn.tsx
@@ -2,30 +2,46 @@ import { StyleSheet, TouchableOpacity, View } from "react-native"
 import Icon from '@expo/vector-icons/SimpleLineIcons';
 import { selectColors } from "../theme";
 import { useAppSelector } from "../app/hooks";
+import { useState } from "react";
+import { Menu, MenuItem } from 'react-native-material-menu';
 
 export const KebabBtn = () => {
+  const [visible, setVisible] = useState(false);
   const colors = useAppSelector(selectColors);
+
+  const hideMenu = () => setVisible(false);
+
+  const showMenu = () => setVisible(true);
 
   return (
     <View
       style={customStyles.botonContainer}
     >
-        <TouchableOpacity
+      <Menu
+        visible={visible}
+        onRequestClose={hideMenu}
+      >
+        <MenuItem onPress={hideMenu}>Editar</MenuItem>
+        <MenuItem onPress={hideMenu}>Atendido</MenuItem>
+        <MenuItem onPress={hideMenu}>Eliminar</MenuItem>
+      </Menu>
+
+      <TouchableOpacity
+        style={{
+            ...customStyles.boton,
+          }}
+        onPress={() => showMenu()}
+      >
+        {(
+        <Icon
+          name={'options-vertical'}
           style={{
-              ...customStyles.boton,
-            }}
-          onPress={() => {}}
-        >
-          {(
-          <Icon
-            name={'options-vertical'}
-            style={{
-              fontSize: 22,
-              color: colors.text
-            }}
-          />
-        )}
-        </TouchableOpacity>
+            fontSize: 22,
+            color: colors.text
+          }}
+        />
+      )}
+      </TouchableOpacity>
     </View>
   )
 }
