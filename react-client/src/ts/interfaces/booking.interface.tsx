53d27067f274eb398720ea2aa67b4d9736ca32da
jwilla <140156@unsaac.edu.pe>
2023-01-15 14:35:30 -0500

    contactId

--- react-client/src/ts/interfaces/booking.interface.tsx
+++ react-client/src/ts/interfaces/booking.interface.tsx
@@ -1,7 +1,7 @@
 export interface BookingInterface {
   startdate: Date,
   tours: {name: String, _id: String}[],
-  contact: {name: String, _id: String},
+  contactId: String,
   ntravelers: Number,
   state: "Pending" | "In Progress" | "Completed" | "Cancelled",
   price: Number,
