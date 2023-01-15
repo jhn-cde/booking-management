9d5e95623005b2ba54d8dc85b01ed0bb73d4331c
jwilla <140156@unsaac.edu.pe>
2023-01-15 14:28:25 -0500

    fill booking, contact

--- react-client/src/features/booking/page/Booking.tsx
+++ react-client/src/features/booking/page/Booking.tsx
@@ -2,23 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native';
 import { RootStackScreenProps } from '../../../navigators/types';
 import { PageContainer, Refresh } from '../../../components';
-import { getBooking, getTour } from '../../../api'
 import { BookingInterface } from '../../../ts/interfaces/booking.interface';
+import { CustomerInterface } from '../../../ts/interfaces/customer.interface';
+import { get } from '../../../api/api';
 
 export const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) => {
   const [booking, setBooking] = useState<BookingInterface | undefined>(undefined)
-
+  const [contact, setContact] = useState<CustomerInterface | undefined>(undefined)
+  
   useEffect(()=>{
-    getBooking(route.params.id, setBooking)
+    fillData();
   }, [])
   
-  if(!booking) return(
-    <Refresh refreshFun={getTour(route.params.id, setBooking)}/>
-  )
+  async function fillData() {
+    const _booking = await get('bookings', route.params.id)
+    setBooking(_booking)
+    if(_booking){
+      const _contact = await get('customers', _booking.contactId)
+      setContact(_contact)
+    }
+  }
 
+  if(!booking || !contact) return(
+    <Refresh refreshFun={fillData}/>
+  )
+  /* tour, nombres*, telefono*, lugar*, nViajantes*, fecha*, extra* */
   return (
     <PageContainer>
-      <Text>{booking.contact.name}</Text>
+      <Text>{contact.name}</Text>
     </PageContainer>
   )
 }
