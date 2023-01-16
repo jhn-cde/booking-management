796110d034f3d76b70c76607fd617f0da6a70bf2
jwilla <140156@unsaac.edu.pe>
2023-01-16 18:34:03 -0500

    address, extra added

--- react-client/src/ts/interfaces/booking.interface.tsx
+++ react-client/src/ts/interfaces/booking.interface.tsx
@@ -3,10 +3,12 @@ export interface BookingInterface {
   tours: {name: String, _id: String}[],
   contactId: String,
   ntravelers: Number,
+  address: String,
   state: "Pending" | "In Progress" | "Completed" | "Cancelled",
   price: Number,
   foodType: String,
   travelers: {_id: String}[],
+  extra: String,
   userId: String,
   _id: String
 }
