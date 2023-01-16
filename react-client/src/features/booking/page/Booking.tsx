853b40f35207b4c230dbf1f6f6db216bfff3895e
jwilla <140156@unsaac.edu.pe>
2023-01-16 18:34:53 -0500

    all components added

--- react-client/src/features/booking/page/Booking.tsx
+++ react-client/src/features/booking/page/Booking.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { RootStackScreenProps } from '../../../navigators/types';
-import { PageContainer, Refresh } from '../../../components';
-import { BookingInterface, CustomerInterface } from '../../../ts/interfaces';
+import { PageContainer, Refresh, TextInputWithIcon } from '../../../components';
+import { TourInterface } from '../../../ts/interfaces';
 import { get } from '../../../api/api';
+import useForm from '../../../hooks/useForm';
+import { DateWithIcon, DropDownWithIcon } from '../components';
 
 export const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) => {
-  const [booking, setBooking] = useState<BookingInterface | undefined>(undefined)
-  const [contact, setContact] = useState<CustomerInterface | undefined>(undefined)
+  const [booking, handleBookingChange, setBooking] = useForm(undefined)
+  const [contact, handleContactChange, setContact] = useForm(undefined)
+  const [tour, handleTourChange, setTour] = useForm(undefined)
   
   useEffect(()=>{
     fillData();
@@ -17,18 +20,70 @@ export const Booking = ({route, navigation}: RootStackScreenProps<'Booking'>) =>
     const _booking = await get('bookings', route.params.id)
     setBooking(_booking)
     if(_booking){
-      const _contact = await get('customers', _booking.contactId)
-      setContact(_contact)
+      try {
+        const _contact = await get('customers', _booking.contactId)
+        setContact(_contact)
+
+        const _tour = await get('tours', _booking.tours[0]._id)
+        setTour(_tour)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
-  if(!booking || !contact) return(
+  if(!booking || !contact || !tour) return(
     <Refresh refreshFun={fillData}/>
   )
-  /* tour, nombres*, telefono*, lugar*, nViajantes*, fecha*, extra* */
   return (
     <PageContainer>
-      <Text>{contact.name}</Text>
+      <View>
+        <Text>Sobre el tour</Text>
+        <View>
+          <DropDownWithIcon
+            name={'Tour'}
+            tour={tour} 
+            setTour={(_tour: TourInterface)=>setTour(_tour)}
+          />
+          <TextInputWithIcon
+            name={'N. Pasajeros'}
+            text={booking.ntravelers.toString()} 
+            setText={(_text: string)=>handleBookingChange({k: 'ntravelers', v: _text})}
+          />
+          <DateWithIcon
+            name={'Fecha'}
+            date={booking.startdate} 
+            setDate={(_date: Date)=>handleBookingChange({k: 'startdate', v: _date})}
+          />
+          <TextInputWithIcon
+            name={'Extra'}
+            text={booking.extra.toString()} 
+            setText={(_text: string)=>handleBookingChange({k: 'extra', v: _text})}
+          />
+        </View>
+      </View>
+
+      <View>
+        <Text>Datos de contacto</Text>
+        <View>
+          <TextInputWithIcon
+            name={'Nombres'}
+            text={contact.name.toString()} 
+            setText={(_text: string)=>handleContactChange({k: 'name', v: _text})}
+          />
+          <TextInputWithIcon
+            name={'Telefono'}
+            text={contact.phone.toString()} 
+            setText={(_text: string)=>handleContactChange({k: 'phone', v: _text})}
+          />
+
+          <TextInputWithIcon
+            name={'Direccion'}
+            text={booking.address.toString()} 
+            setText={(_text: string)=>handleBookingChange({k: 'address', v: _text})}
+          />
+        </View>
+      </View>
     </PageContainer>
   )
 }
