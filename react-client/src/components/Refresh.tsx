a672ceca013b326b9e28296c9f1768e7bea4c926
jwilla <140156@unsaac.edu.pe>
2023-01-17 11:19:04 -0500

    show custom text

--- react-client/src/components/Refresh.tsx
+++ react-client/src/components/Refresh.tsx
@@ -4,31 +4,34 @@ import Icon from '@expo/vector-icons/Ionicons';
 import { selectColors } from '../theme';
 import { useAppSelector } from '../app/hooks';
 
-export const Refresh = ({refreshFun}:{refreshFun: any}) => {
+export const Refresh = ({refreshFun, text}:{refreshFun: any, text?: String}) => {
   const colors = useAppSelector(selectColors);
   return (
-    <View
-      style={{
-        flexDirection:'row', 
-        justifyContent:'space-between', 
-        width:150, 
-        alignItems:'center',
-        marginVertical: 20
-      }}
-    >
-      <Text
-        style={{color:colors.text}}
-      >Cargando...</Text>
-      <TouchableOpacity
-        onPress={refreshFun}
+    <View>
+      {text&&<Text>{text}</Text>}
+      <View
+        style={{
+          flexDirection:'row', 
+          justifyContent:'space-between', 
+          width:150, 
+          alignItems:'center',
+          marginVertical: 20
+        }}
       >
-        <Icon
-          name="refresh" 
-          color={colors.card} 
-          size={26}
-          style={{textDecorationLine: 'underline'}}
-        />
-      </TouchableOpacity>
+        <Text
+          style={{color:colors.text}}
+        >Recargar...</Text>
+        <TouchableOpacity
+          onPress={refreshFun}
+        >
+          <Icon
+            name="refresh" 
+            color={colors.card} 
+            size={26}
+            style={{textDecorationLine: 'underline'}}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
